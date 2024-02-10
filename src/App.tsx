069bc1ca7972a1063ebc123f2bd3b2c65ca77140
SageM <madduxsage@gmail.com>
2024-02-09 21:15:42 -0500

    Added tour card component wrapped in container using paper and grid inside

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 import { useState } from 'react'
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import {Container} from '@mui/material';
+import TourCard from './components/TourCard';
+import {Grid} from '@mui/material';
+
 
 
 
@@ -7,8 +13,15 @@ function App() {
 
   return (
     <>
-    shalom!
-
+    <Container maxWidth="sm">
+      <Grid container spacing={4}>
+        <TourCard/>
+        <TourCard/>
+        <TourCard/>
+        <TourCard/>
+      </Grid>
+      {/* <Button variant="contained">Hello world</Button> */}
+    </Container>
     </>
   )
 }
---
+++ src/components/TourCard.tsx
@@ -0,0 +1,12 @@
+import Paper from '@mui/material/Paper';
+import {Grid} from '@mui/material';
+
+const TourCard = () => {
+    return (
+        <Grid item xs={3}>
+            <Paper elevation={3}>Hello tour</Paper>
+        </Grid>
+    )
+}
+
+export default TourCard
