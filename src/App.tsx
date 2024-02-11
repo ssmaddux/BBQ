7d9086caab6f5d4e694e4a120af27a845c96aa6d
SageM <madduxsage@gmail.com>
2024-02-10 21:22:48 -0500

    Added app bar with drawer and text for place holder

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import { useState } from 'react'
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import {Container} from '@mui/material';
+import {AppBar} from '@mui/material';
 import TourCard from './components/TourCard';
 import {Grid} from '@mui/material';
+import SearchAppBar from './components/AppBar';
+import DrawerAppBar from './components/AppBar';
 
 
 
@@ -13,6 +16,7 @@ function App() {
 
   return (
     <>
+    <DrawerAppBar/>
     <Container maxWidth="sm">
       <Grid container spacing={1}>
         <TourCard/>
